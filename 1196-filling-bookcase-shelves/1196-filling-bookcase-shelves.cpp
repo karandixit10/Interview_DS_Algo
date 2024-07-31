bc58579cb35a30e2c767cf818b6b0973c123b658
Karan Dixit <33357804+karandixit10@users.noreply.github.com>
2024-07-31 07:42:11 +0530

    Time: 4 ms (60.3%), Space: 31.7 MB (5.37%) - LeetHub

---
+++ 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    int solve(vector<vector<int>>& books, int i, int remShelfWidth,
+              int maxShelfHeight, int shelfWidth, vector<vector<int>>& dp) {
+        if (i == books.size()) {
+            return maxShelfHeight;
+        }
+
+        if (dp[i][remShelfWidth] != -1)
+            return dp[i][remShelfWidth];
+        int currShelf = INT_MAX, nextShelf = INT_MAX;
+        int bookWidth = books[i][0];
+        int bookHeight = books[i][1];
+
+        // Case 1: keep the book in current shelf
+        if (bookWidth <= remShelfWidth) {
+            currShelf = solve(books, i + 1, remShelfWidth - bookWidth,
+                              max(maxShelfHeight, bookHeight), shelfWidth, dp);
+        }
+        // Case 2: keep the book in next shelf
+        nextShelf = maxShelfHeight + solve(books, i + 1, shelfWidth - bookWidth,
+                                           bookHeight, shelfWidth, dp);
+        return dp[i][remShelfWidth] = min(currShelf, nextShelf);
+    }
+
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        vector<vector<int>> dp(1001, vector<int>(shelfWidth + 1, -1));
+        return solve(books, 0, shelfWidth, 0, shelfWidth, dp);
+    }
+};
