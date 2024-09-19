7d6ec17071e348bba6d1a542f7e2818de1dc579f
Karan Dixit <33357804+karandixit10@users.noreply.github.com>
2024-09-19 17:32:30 +0530

    Time: 4 ms (46.04%), Space: 13.9 MB (24.12%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,53 +1,34 @@
 class Solution {
 private:
-    vector<int> memoization(string& expression, vector<vector<vector<int>>>& dp,
-                            int start, int end) {
-        vector<int> results;
-        if (!dp[start][end].empty()) {
-            return dp[start][end];
-        }
+    vector<int> solve(string s) {
+        vector<int> result;
 
-        if (start == end) {
-            results.push_back(expression[start] - '0');
-            return results;
-        }
-        if (end - start == 1 && isdigit(expression[start])) {
-            int tens = expression[start] - '0';
-            int ones = expression[end] - '0';
-            results.push_back(10 * tens + ones);
-            return results;
-        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '+' || s[i] == '-' || s[i] == '*') {
+                vector<int> left_results = solve(s.substr(0, i));
+                vector<int> right_results = solve(s.substr(i + 1));
 
-        for (int i = start; i <= end; i++) {
-            char currentChar = expression[i];
-            if (isdigit(currentChar)) {
-                continue;
-            }
-            vector<int> leftResults  = memoization(expression, dp, start, i - 1);
-            vector<int> rightResults = memoization(expression, dp, i + 1, end);
-            for (int leftValue : leftResults) {
-                for (int rightValue : rightResults) {
-                    switch (currentChar) {
-                    case '+':
-                        results.push_back(leftValue + rightValue);
-                        break;
-                    case '-':
-                        results.push_back(leftValue - rightValue);
-                        break;
-                    case '*':
-                        results.push_back(leftValue * rightValue);
-                        break;
+                for (int& x : left_results) {
+                    for (int& y : right_results) {
+                        if (s[i] == '+') {
+                            result.push_back(x + y);
+                        } else if (s[i] == '-') {
+                            result.push_back(x - y);
+                        } else {
+                            result.push_back(x * y);
+                        }
                     }
                 }
             }
         }
-        return dp[start][end] = results;
+
+        if (result.empty()) {
+            result.push_back(stoi(s));
+        }
+
+        return result;
     }
 
 public:
-    vector<int> diffWaysToCompute(string expression) {
-        int n = expression.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n));
-        return memoization(expression, dp, 0, expression.length() - 1);
-    }
+    vector<int> diffWaysToCompute(string s) { return solve(s); }
 };
